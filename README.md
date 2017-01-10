2ef0c215ce21166f49ab55a784e0ece7c55ebbe6
Max Maton <max@maton.info>
2017-01-10 12:26:52 +0000

    Version 0.2

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Configures fusiondirectory
 * `$ldap_host` - The ldap uri for the ldp server
 * `$ldap_admin_dn` - The full dn of the admin user
 * `$ldap_admin_pass` - The pass of the admin user
+* `$fusiondirectory_admin_cn` The username of the fusiondirectory administrator. Defaults to: 'administrator'
+* `$fusiondirectory_admin_pass` The password of the fusiondirectory administrator
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,32 @@ class fusiondirectory(
     $ldap_base,
     $ldap_admin_dn,
     $ldap_admin_pass,
+    $fusiondirectory_admin_cn = 'administrator',
+    $fusiondirectory_admin_pass,
   ){
+
+  if ! defined(Apt::Source['fusiondirectory']) {
+    apt::source { 'fusiondirectory':
+      comment  => 'Fusiondirectory repository',
+      location => 'http://repos.fusiondirectory.org/debian-jessie/',
+      key      => {
+        'id'     => '5636FC267B643B144F31BCA0E184859262B4981F',
+        'server' => 'keys.gnupg.net',
+      },
+    }
+  }
+
   package { 'fusiondirectory':
-    ensure => present,
+    ensure  => present,
+    require => Apt::Source['fusiondirectory'],
   }
 
   package { 'fusiondirectory-plugin-systems':
+    ensure  => present,
+    require => Package['fusiondirectory'],
+  }
+  
+  package { 'fusiondirectory-plugin-puppet':
     ensure  => present,
     require => Package['fusiondirectory'],
   }
@@ -23,5 +43,44 @@ class fusiondirectory(
       'ldap_admin_pass' => $ldap_admin_pass,
     }),
     replace => false,
+    mode    => '640',
+    notify  => Exec['/usr/sbin/fusiondirectory-setup --yes --check-ldap'],
+  }
+
+  ldap::object { "ou=people,${ldap_base}":
+    attributes => {
+      'ou' => 'people',
+      'objectClass' => [
+        'organizationalUnit',
+      ],
+    },
+    ensure => present,
+    adduser => $ldap_admin_dn,
+    addpw => $ldap_admin_pass,
+  }
+
+  ldap::object { "uid=${fusiondirectory_admin_cn},ou=people,${ldap_base}":
+    attributes => {
+      'cn'          => $fusiondirectory_admin_cn,
+      'objectClass' => [
+        'inetOrgPerson',
+        'organizationalPerson',
+        'person',
+        'top',
+      ],
+      'userPassword'    => $fusiondirectory_admin_pass,
+      'sn'          => 'Administrator',
+      'uid'         => $fusiondirectory_admin_cn,
+    },
+    ensure => present,
+    adduser => $ldap_admin_dn,
+    addpw => $ldap_admin_pass,
+    require => Ldap::Object["ou=people,${ldap_base}"],
+  }
+
+  exec { '/usr/sbin/fusiondirectory-setup --yes --check-ldap':
+    command     => "/bin/bash -c '/usr/sbin/fusiondirectory-setup --yes --check-ldap <<< ${fusiondirectory_admin_cn}'",
+    refreshonly => true,
+    require => [File["/etc/fusiondirectory/fusiondirectory.conf"], Ldap::Object["uid=${fusiondirectory_admin_cn},ou=people,${ldap_base}"]],
   }
 }
